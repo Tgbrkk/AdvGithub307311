de0116938fa13bae6bfce8e77cf6cf0fa83c9afd
Tuğberk BİLEN <tgbrkblnn@gmail.com>
2025-02-07 20:47:32 +0300

    versiyon2

--- src/MyClass.java
+++ src/MyClass.java
@@ -3,5 +3,7 @@ public class MyClass {
     public static void main(String[] args) {
         //değişiklik 1
         System.out.println("Herkese selamlar:");
+        //değişiklik 2
+        System.out.println("Advance GitHub dersine hoşgeldiniz");
     }
 }
